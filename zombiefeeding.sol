ec26ed05abc68b531d3148ad46002dc589d3e858
Fred-Reis <fredgost@hotmail.com>
2022-06-03 22:19:40 -0300

    kitty interface added and level 2 finished

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -23,11 +23,27 @@ interface KittyInterface {
 }
 
 contract ZombieFeeding is ZombieFactory {
-    function feedAndMultiply(uint256 _zombieId, uint256 _targetDna) public {
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
+    function feedAndMultiply(
+        uint256 _zombieId,
+        uint256 _targetDna,
+        string memory _species
+    ) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint256 newDna = (myZombie.dna + _targetDna) / 2;
+        if (keccak256(abi.encodePacked(_species)) == keccak256("kitty")) {
+            newDna = newDna - (newDna % 100) + 99;
+        }
         _createZombie("NoName", newDna);
+    }
+
+    function feedOnKitty(uint256 _zombieId, uint256 _kittyId) public {
+        uint256 kittyDna;
+        (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 }
